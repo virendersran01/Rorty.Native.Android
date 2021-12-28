de6becb4197926417de4d93e251ace3ebff9648d
developersancho <developersancho@gmail.com>
2021-12-28 23:05:50 +0300

    [DEV][Built buildSrc]

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,3 +7,21 @@ repositories {
     mavenCentral()
     gradlePluginPortal()
 }
+
+object Versions {
+    const val GRADLE = "7.1.0-beta05"
+    const val KOTLIN = "1.6.10"
+    const val HILT = "2.40.1"
+}
+
+object Deps {
+    const val ANDROID_GRADLE = "com.android.tools.build:gradle:${Versions.GRADLE}"
+    const val KOTLIN_GRADLE = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}"
+    const val HILT_GRADLE = "com.google.dagger:hilt-android-gradle-plugin:${Versions.HILT}"
+}
+
+dependencies {
+    implementation(Deps.ANDROID_GRADLE)
+    implementation(Deps.KOTLIN_GRADLE)
+    implementation(Deps.HILT_GRADLE)
+}
